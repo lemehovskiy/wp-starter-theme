cf50aaec3ef23ab3295e9015960de73d7587517d
lemehovskiy <lemehovskiy@gmail.com>
2017-11-30 17:49:36 +0200

    page wrap

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
+</div><!-- #content -->
+
 <footer>
     <div class="copyright">
         &copy; <?php echo date('Y') ?>
     </div>
 </footer>
+
+</div><!-- #page -->
 
 <?php wp_footer(); ?>
 </body>
