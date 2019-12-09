7e282fd5f48c3f5907b13d3afe04b3418e696473
lemehovskiy <lemehovskiy@gmail.com>
2019-12-09 22:13:17 +0200

    update readme

--- README.md
+++ README.md
@@ -1,11 +1,71 @@
-### Fontello open
+## Fontello scripts
+
+### open
 
 ```sh
 make -f fontello
 ```
 
-### Fontello save
+### save
 
 ```sh
 make -f fontello fontsave
 ```
+
+
+##PLOP Micro-generator framework
+
+### Init builder
+
+```sh
+plop builder {{builder-name}} 
+```
+
+#### In result you should have structure like this:
+
+     ├── ...
+     └── wp-starter-theme
+         ├── ...
+         └── src
+             ├── ...
+             └── sass
+                 ├── ...
+                 ├── {{builder-name}}-builder
+                 │   └── {{builder-name}}-builder.scss
+                 └── ...
+     
+##### And import {{builder-name}}-builder.scss to style.scss
+
+### Builder section
+
+```sh
+plop section {{section-name}} {{builder-name}} 
+```
+
+#### In result you should have structure like this:
+
+##### SASS:
+
+     ├── ...
+     └── wp-starter-theme
+         ├── ...
+         └── src
+             ├── ...
+             └── sass
+                 ├── ...
+                 ├── {{builder-name}}-builder
+                 |   ├── {{builder-name}}-builder.scss
+                 │   └── {{section-name}}-intro-1.scss
+                 └── ...
+     
+##### And import {{section-name}}-builder.scss to {{builder-name}}-builder.scss
+
+##### PHP:
+
+     ├── ...
+     └── wp-starter-theme
+         ├── ...
+         └── template-parts
+             ├── ...
+             └── {{builder-name}}-builder
+                 └── section-{{section-name}}.php
--- plopfile.js
+++ plopfile.js
@@ -2,7 +2,7 @@ const importPlaceholder = '//PLOP will add new import here (DO NOT REMOVE)';
 const importPlaceholderPattern = /\/\/PLOP will add new import here \(DO NOT REMOVE\)/g;
 
 module.exports = function (plop) {
-    plop.setGenerator('init builder', {
+    plop.setGenerator('builder', {
         description: 'init ACF flexible content builder',
         prompts: [{
             type: 'input',
@@ -20,6 +20,39 @@ module.exports = function (plop) {
                 path: 'src/sass/style.scss',
                 pattern: importPlaceholderPattern,
                 template: `@import "{{name}}-builder/{{name}}-builder.scss"; \r\n${importPlaceholder}`,
+            }
+        ]
+    });
+    plop.setGenerator('section', {
+        description: 'create ACF flexible content section',
+        prompts: [
+            {
+                type: 'input',
+                name: 'sectionName',
+                message: 'section name please'
+            },
+            {
+                type: 'input',
+                name: 'builderName',
+                message: 'builder name please'
+            }
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: 'src/sass/{{builderName}}-builder/section-{{sectionName}}.scss',
+                templateFile: 'plop-templates/builder-section.scss.hbs'
+            },
+            {
+                type: 'add',
+                path: 'template-parts/{{builderName}}-builder/section-{{sectionName}}.php',
+                templateFile: 'plop-templates/builder-section.php.hbs'
+            },
+            {
+                type: 'modify',
+                path: 'src/sass/{{builderName}}-builder/{{builderName}}-builder.scss',
+                pattern: importPlaceholderPattern,
+                template: `@import "section-{{sectionName}}"; \r\n${importPlaceholder}`,
             }
         ]
     });
