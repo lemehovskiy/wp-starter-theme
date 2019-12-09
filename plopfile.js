c8708d8bfb2268fb6be418ac53cb3c1b1ab63fcf
lemehovskiy <lemehovskiy@gmail.com>
2019-12-09 21:08:59 +0200

    builder generator

--- plopfile.js
+++ plopfile.js
@@ -1,16 +1,26 @@
+const importPlaceholder = '//PLOP will add new import here (DO NOT REMOVE)';
+const importPlaceholderPattern = /\/\/PLOP will add new import here \(DO NOT REMOVE\)/g;
+
 module.exports = function (plop) {
-    // controller generator
-    plop.setGenerator('controller', {
-        description: 'application controller logic',
+    plop.setGenerator('init builder', {
+        description: 'init ACF flexible content builder',
         prompts: [{
             type: 'input',
             name: 'name',
-            message: 'controller name please'
+            message: 'builder name please'
         }],
-        actions: [{
-            type: 'add',
-            path: 'src/sass/page_builder/section-{{name}}.js',
-            templateFile: 'plop-templates/section.scss.hbs'
-        }]
+        actions: [
+            {
+                type: 'add',
+                path: 'src/sass/{{name}}-builder/{{name}}-builder.scss',
+                templateFile: 'plop-templates/builder-index.scss.hbs'
+            },
+            {
+                type: 'modify',
+                path: 'src/sass/style.scss',
+                pattern: importPlaceholderPattern,
+                template: `@import "{{name}}-builder/{{name}}-builder.scss"; \r\n${importPlaceholder}`,
+            }
+        ]
     });
 };
